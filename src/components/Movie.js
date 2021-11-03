64d3b9fb5c6eb021c041c1e501b075c3c0b79ebc
lantaole <lantaole>
2021-11-03 16:47:55 +0800

    coding finish

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -7,9 +7,9 @@ const Movie = ({ movie }) => {
   const poster = movie.Poster === 'N/A' ? DEFAULT_PLACEHOLDER_IMAGE : movie.Poster;
   return (
     <div className="movie">
-      <h2>{movie.title}</h2>
+      <h2>{movie.Title}</h2>
       <div>
-        <img width='200'  src={poster} alt={`The Move titled: ${movie.title}`}/>
+        <img width='200'  src={poster} alt={`The Move titled: ${movie.Title}`}/>
       </div>
       <p>({movie.Year})</p>
     </div>
